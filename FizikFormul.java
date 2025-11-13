c2ec12d517ec18d843e82d941b15eb945cfbd688
Seha83 <250541021@firat.edu.tr>
2025-11-13 21:40:38 +0300

    Update FizikFormul.java

--- FizikFormul.java
+++ FizikFormul.java
@@ -1,7 +1,7 @@
 /*
- * Ad Soyad: [ADINIZI BURAYA YAZIN]
- * Ogrenci No: [OGRENCI NUMARANIZI BURAYA YAZIN]
- * Tarih: [TARIHI BURAYA YAZIN]
+ * Ad Soyad: [İbrahim Seha DOĞAN]
+ * Ogrenci No: [250541021]
+ * Tarih: [13.11.2025]
  * Aciklama: Gorev 2 - Fizik Formulu Asistani
  *
  * Bu program temel fizik formullerini kullanarak
